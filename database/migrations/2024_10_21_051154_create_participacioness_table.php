e0f11b4a08ba5dc457cb3da8d2677047ae2618fb
Santiago Home <itzdarkoxdyt@gmail.com>
2024-10-21 00:17:24 -0500

    añadi las migraciones de las 3 tablas

---
+++ database/migrations/2024_10_21_051154_create_participacioness_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('participacioness', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('evento_id');
+            $table->unsignedBigInteger('organizador_id');
+            $table->foreign('organizador_id')->references('id')->on('_eventos')->onDelete('cascade');
+            $table->foreign('evento_id')->references('id')->on('_organizadores')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('participacioness');
+    }
+};
